2346dfa07ced900de45d82f7e9e9a98a4385bdf1
David Gomez-Urquiza <david.gurquiza@gmail.com>
2021-03-22 23:08:59 -0600

    Update to twitterVersion 20.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val twitterVersion = "19.10.0"
+  val twitterVersion = "20.10.0"
   
   lazy val twitterServer = "com.twitter" %% "twitter-server" % twitterVersion
   lazy val finagleStats = "com.twitter" %% "finagle-stats" % twitterVersion
